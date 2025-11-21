39b9f9ad511d15a289eec975203f0b51d4160e1c
CesarZ <zcesarrr.tw@gmail.com>
2025-11-21 16:33:20 -0700

    Update send_msg.php

--- server/send_msg.php
+++ server/send_msg.php
@@ -2,8 +2,11 @@
 require_once 'db.php';
 $pdo = start_db();
 
-query($pdo, "INSERT INTO messages(msg) VALUES(?);", ["holaaaauwhads"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $message = $_POST["message"];
+    query($pdo, "INSERT INTO messages(msg) VALUES(?);", [$message]);
 
-$messages_rows = query_notparams($pdo, "SELECT * FROM messages");
-print_rows($messages_rows, ["id", "msg"]);
+    $messages_rows = query_notparams($pdo, "SELECT * FROM messages");
+    print_rows($messages_rows, ["id", "msg"]);
+}
 ?>
