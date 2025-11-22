aedcdb5158d510fd24cd993ffe8433c9f6cb470a
CesarZ <zcesarrr.tw@gmail.com>
2025-11-21 20:57:51 -0700

    Update send_msg.php

--- server/send_msg.php
+++ server/send_msg.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'db.php';
 header("Content-Type: application/json; chatset=utf-8");
 
 $input = json_decode(file_get_contents("php://input"), true);
@@ -10,17 +9,26 @@ if (!$input) {
     exit;
 }
 
+
+// Get data
 $msg = trim($input["msg"] ?? '');
 
+
+// Validations
 if (strlen($msg) < 6) {
     http_response_code(422);
     echo json_encode(["status" => "error", "message" => "The message cannot be short!"]);
     exit;
 }
 
+
+// Do operations
+require_once 'db.php';
 $pdo = initDB();
 query($pdo, "INSERT INTO messages(msg) VALUES(?);", [$msg]);
 
+
+// Return response
 $responseData = [
     "sentMessage" => $msg
 ];
