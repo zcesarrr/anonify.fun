640856822a853043b77fc5e811678e1b628530a0
CesarZ <zcesarrr.tw@gmail.com>
2025-11-22 14:34:45 -0700

    Update _lastMessagesHandle.js

--- scripts/_lastMessagesHandle.js
+++ scripts/_lastMessagesHandle.js
@@ -1,18 +1,18 @@
-/*const lastMessagesTemplate = document.getElementById("lastMessagesTemplate");
-
-const lastMessagesContainer = document.getElementById("lastMessagesContainer");
-
-const clonedContent = lastMessagesTemplate.content.cloneNode(true);
-
-lastMessagesContainer.appendChild(clonedContent);*/
-
 const lastMessagesContainer = document.createElement("div");
+lastMessagesContainer.setAttribute("id", "lastMessagesContainer")
 lastMessagesContainer.innerHTML = `
     <br><hr>
-    <div id="lastMessagesContent">
-        <button>Show Last Anonymous Messages</button>
-        <div id="lastMessagesShower"></div>
+    <button id="showLastMessagesBtn">Show Last Anonymous Messages</button>
+    <div id="lastMessagesContent"></div>
     </div>
 `;
 
 document.body.appendChild(lastMessagesContainer);
+
+const showLastMessagesBtn = document.getElementById("showLastMessagesBtn");
+
+showLastMessagesBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+
+   
+});
