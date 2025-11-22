1a747933e571ded864f358037fb16e2e872e35a3
CesarZ <zcesarrr.tw@gmail.com>
2025-11-21 20:35:41 -0700

    submit message javascript

---
+++ scripts/submitMessage.js
@@ -0,0 +1,37 @@
+const messageForm = document.getElementById("messageForm");
+const result = document.getElementById("result");
+
+messageForm.addEventListener("submit", async(e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const payload = {
+        msg: formData.get("message")
+    };
+
+    result.textContent = "Sending...";
+
+    try {
+        const res = await fetch("../server/send_msg.php", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        });
+
+        if (!res.ok) {
+            const err = await res.json().catch(() => null);
+            result.textContent("Server error: " + (err?.message || res.statusText));
+            return;
+        }
+
+        const data = await res.json();
+        if (data.status === "success") {
+            result.innerHTML = data.message + '<br>message: ' + (data.data?.message || '');
+        } else {
+            result.textContent = (data.message || "Uknown error");
+        }
+    } catch (err) {
+        console.error(err);
+        result.textContent = "Unable connect to server";
+    }
+});
