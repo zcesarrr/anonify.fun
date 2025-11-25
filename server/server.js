bdb8d58e6aed6f53c670f0f1d461bf9ccf34316b
CesarZ <zcesarrr.tw@gmail.com>
2025-11-25 13:42:40 -0700

    env setup

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
