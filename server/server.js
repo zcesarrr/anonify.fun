b15e7941f298409e5c821ddc1d7a41c407de223b
CesarZ <zcesarrr.tw@gmail.com>
2025-11-25 14:05:23 -0700

    adapting

--- server/server.js
+++ server/server.js
@@ -4,15 +4,28 @@ const port = 3000;
 
 const pool = require('./config/db');
 
-app.get('/', async(req, res) => {
+app.get('/messages/', async(req, res) => {
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM messages ORDER BY id DESC');
         client.release();
-        res.json(result.rows);
+        
+        const data = {
+            status: "success",
+            message: "Operation has been completed sucessfully!",
+            data: result.rows
+        }
+        
+        res.status(200).json(data);
     } catch (err) {
         console.error("Error executing query", err);
-        res.status(500).send("Server error");
+
+        const data = {
+            status: "error",
+            message: "Server error"
+        }
+
+        res.status(500).json(data);
     }
 });
 
