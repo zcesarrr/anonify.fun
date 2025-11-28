10fc8914dd605fb12a4b74ce07903b96b83d9958
CesarZ <zcesarrr.tw@gmail.com>
2025-11-28 10:28:20 -0700

    triggers crated

--- sql/db.sql
+++ sql/db.sql
@@ -14,11 +14,28 @@ SELECT * FROM messages;
 
 SELECT * FROM messages ORDER BY created_at DESC LIMIT 10;
 
-UPDATE messages SET answer = 'Hola gracias por responder' WHERE id = 'e42a25d6-5176-43e0-b260-a2ce1dbc30c1';
+UPDATE messages SET answer = 'Gracias por probar :3' 
+WHERE id = '9649fdc5-0a4c-4a58-bc09-2f0d221160fd';
 
 -- Test searching message by id
 SELECT * FROM messages WHERE id = 'e132b481-4d75-46a0-a14d-622b1401ff2b';
-SELECT * FROM messages WHERE answer IS NOT NULL;
+SELECT * FROM messages WHERE answer IS NOT NULL ORDER BY created_at DESC LIMIT 10;
+SELECT * FROM messages WHERE answer IS NULL ORDER BY created_at DESC LIMIT 10;
 
 INSERT INTO messages(msg) VALUES('hi owo');
 INSERT INTO messages(msg) VALUES('hello :3');
+
+-- Triggers
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.answer_updated_at = now();
+	RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_your_table_modified_time
+BEFORE UPDATE OF answer ON messages
+FOR EACH ROW
+WHEN (OLD.answer IS DISTINCT FROM new.answer)
+EXECUTE FUNCTION update_modified_column();
