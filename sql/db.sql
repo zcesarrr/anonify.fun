0bbd6b49bff49f8047cb9334afdfd5b2f10b84e4
CesarZ <zcesarrr.tw@gmail.com>
2025-11-28 22:56:51 -0700

    SQL queries organized

--- sql/db.sql
+++ sql/db.sql
@@ -1,39 +1,23 @@
+-- EXTREME EMERGENCY
 DROP TABLE messages;
 
+
+-- Create UUID4 extension only for the first installation
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+
+-- Create messages table only for the first installation
 CREATE TABLE messages(
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	msg TEXT NOT NULL,
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 	answer TEXT NULL,
+	answered_at TIMESTAMP WITH TIME ZONE NULL,
 	answer_updated_at TIMESTAMP WITH TIME ZONE NULL
 );
 
-ALTER TABLE messages
-ADD COLUMN answered_at TIMESTAMP WITH TIME ZONE NULL;
 
-SELECT * FROM messages;
-
-SELECT * FROM messages ORDER BY created_at DESC LIMIT 10;
-
-UPDATE messages SET answered_at = CURRENT_TIMESTAMP;
-
-UPDATE messages SET answer = 'Y que tal? Funciona bien? y esa risa toda pendejita que we' 
-WHERE id = 'edb2c3cb-b0ec-4ecc-9102-ea57587c79d4';
-
--- Test searching message by id
-SELECT * FROM messages WHERE id = 'e132b481-4d75-46a0-a14d-622b1401ff2b';
-SELECT * FROM messages WHERE answer IS NOT NULL ORDER BY created_at DESC LIMIT 10;
-SELECT * FROM messages WHERE answer IS NULL ORDER BY created_at DESC OFFSET 0 ROWS LIMIT 10;
-
-SELECT *, COUNT(*) OVER() AS total_rows FROM messages WHERE answer IS NULL ORDER BY created_at DESC OFFSET 0 LIMIT 10;
-
-
-INSERT INTO messages(msg) VALUES('hi owo');
-INSERT INTO messages(msg) VALUES('hello :3');
-
--- Triggers
+-- Functions
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -47,8 +31,43 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
+
+-- Triggers for the first installation
 CREATE TRIGGER update_your_table_modified_time
 BEFORE UPDATE OF answer ON messages
 FOR EACH ROW
 WHEN (OLD.answer IS DISTINCT FROM new.answer)
 EXECUTE FUNCTION update_modified_column();
+
+
+-- ##########################################################
+
+
+-- Alter table example just in case:
+ALTER TABLE messages
+ADD COLUMN answered_at TIMESTAMP WITH TIME ZONE NULL;
+
+
+-- Get the last 10 messages (all types)
+SELECT * FROM messages ORDER BY created_at DESC LIMIT 10;
+
+
+-- Get the last 10 answered messages
+SELECT *, COUNT(*) OVER() AS total_rows 
+FROM messages WHERE answer IS NOT NULL ORDER BY created_at DESC OFFSET 0 LIMIT 10;
+
+
+-- Get the last 10 NOT-answered messages
+SELECT *, COUNT(*) OVER() AS total_rows 
+FROM messages WHERE answer IS NULL ORDER BY created_at DESC OFFSET 0 LIMIT 10;
+
+
+-- Get all NOT-answered messages
+SELECT *, COUNT(*) OVER() AS total_rows 
+FROM messages WHERE answer IS NULL ORDER BY created_at DESC;
+
+
+-- Answer a message example:
+UPDATE messages 
+SET answer = 'Creo que esto si que va a funcionar... EDIT: Probando updated_at' 
+WHERE id = '407fa152-9adb-4a2f-8fce-169e7ef29cad';
