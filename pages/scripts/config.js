a7f8cdf88e9b2b25ea1b71fb92c8247de4e3eaf3
Cesar Jacob Ramos Paz <zcesarrr.tw@gmail.com>
2025-12-16 13:18:05 -0700

    oops

--- pages/scripts/config.js
+++ pages/scripts/config.js
@@ -3,5 +3,5 @@ const isLocalhost = window.location.hostname === 'localhost' ||
                     window.location.hostname.includes('192.168.');
 
 const config = {
-    baseURL: isLocalhost ? "http://localhost:3000/" : "https://anonify-api.up.railway.app/"
+    apiURL: isLocalhost ? "http://localhost:3000/" : "https://anonify-api.up.railway.app/"
 }
